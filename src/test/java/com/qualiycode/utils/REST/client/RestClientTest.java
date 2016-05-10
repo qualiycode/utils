c11bfa9accb8b38314e00cdc31ce0b762517c038
Eli Rozenfeld <eli.rozenfeld@gmail.com>
2016-05-10 18:37:49 +0300

    restClientTest: updated unit test (old one took more time for nothing)

--- src/test/java/com/qualiycode/utils/REST/client/RestClientTest.java
+++ src/test/java/com/qualiycode/utils/REST/client/RestClientTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import com.qualiycode.utils.pair.Pair;
 import com.qualiycode.utils.strings.DateFormatter;
 import com.qualiycode.utils.strings.StringUtils;
 
@@ -18,51 +17,28 @@ import com.qualiycode.utils.strings.StringUtils;
 public class RestClientTest {
 
 	/**
-	 * Holds the list of applications and their Google Play Store ID's 
-	 */
-	private ArrayList<Pair<String, String>> applications;
-	
-	/**
-	 * Fill list of applications and their Google Play Store ID's
-	 */
-	private void fillApplicationAndIds(){
-		applications = new ArrayList<Pair<String, String>>();
-		applications.add(new Pair<String, String>("WhatsApp", "com.whatsapp"));
-		applications.add(new Pair<String, String>("Facebook messenger", "com.facebook.orca"));
-		applications.add(new Pair<String, String>("Viber", "com.viber.voip"));
-		applications.add(new Pair<String, String>("Skype", "com.skype.raider"));
-	}
-	
-	/**
 	 * This test validate the REST client operation by connecting to google play store and fetching some data
 	 * @throws Exception
 	 */
 	@Test
 	public void restClientTest() throws Exception{
-		fillApplicationAndIds();
 		
 		RestClient rc = new RestClient("play.google.com", 80, RestConnectionType.HTTP);
 
-		for(Pair<String, String> app : applications){
-
-			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			params.add(new BasicNameValuePair("id", app.getSecond()));
-			
-			String output = rc.handleRestCommand("/store/apps/details", RestRequestType.GET, params, null);
-			Assert.assertTrue(rc.getLastStatusCode() == 200);
-			
-			//extracting date & supported OS version
-			String osVersions = StringUtils.getSubstringUsingRegex(output, "operatingSystems.*?\\>\\s+([\\w\\.\\s]+)\\<");
-			String published = StringUtils.getSubstringUsingRegex(output, "datePublished.*?\\>([\\w\\d\\s\\,]+)\\<");
-			
-			published = DateFormatter.getFormattedDate(published, "MMM dd, yyyy", "dd/MM/yyyy");
-			
-			System.out.println("*****************************************");
-			System.out.println("Application = " + app.getFirst());
-			System.out.println("Application Id on Google Play Store = " + app.getSecond());
-			System.out.println("published = " + published);
-			System.out.println("OS versions = " + osVersions);
-		}
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("id", "com.whatsapp"));
+		
+		String output = rc.handleRestCommand("/store/apps/details", RestRequestType.GET, params, null);
+		Assert.assertTrue(rc.getLastStatusCode() == 200);
+		
+		//extracting published date 
+		String published = StringUtils.getSubstringUsingRegex(output, "datePublished.*?\\>([\\w\\d\\s\\,]+)\\<");
+		
+		published = DateFormatter.getFormattedDate(published, "MMM dd, yyyy", "dd/MM/yyyy");
+		
+		System.out.println("Application = " + "Whatsapp");
+		System.out.println("Application Id on Google Play Store = " + "com.whatsapp");
+		System.out.println("published = " + published);
 		
 	}
 
